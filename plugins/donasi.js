fa1ecf1fc61cd0f28bed8b90ed2c8352e341106a
Ryuuzxy <109670909+Ryuuzxy@users.noreply.github.com>
2022-10-04 21:08:09 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
+//let handler = async(m, { conn, text, usedPrefix, command }) => {
+//let pp = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image')
+
+let str = `${global.wm}
+
+┌─「 Donasi • Pulsa 」
+│ • *Indosat:* [${global.ppulsa}]
+❏────
+
+┌─「 Donasi • Non Pulsa 」
+│ • *Dana:* [${global.pdana}]
+│ • *Gopay:* [${global.pgopay}]
+│ • *Ovo:* [${global.povo}]
+❏────`
+let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://Instagram.com/raffuri",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/tv/CgO7Jb9Fkfx/?utm_source=ig_web_copy_link", 
+    title: 'タRyuuBotあ',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sig
+  }
+  } }) 
+          }
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
